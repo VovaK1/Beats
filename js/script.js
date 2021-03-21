92f9ae74739a297fe06ac7f5f4d47b90cf851c3b
Vova Kozak <volodymyr.kozak111@gmail.com>
2021-03-21 12:52:20 +0200

    немного изменил js для меню, не работает

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const fullMenu = document.querySelector(".nav--full");
 const hamburger = document.querySelector(".hamburger");
+fullMenu.style.display = "none";
 
 hamburger.addEventListener('click', e => {
   e.preventDefault();
+  
   fullMenu.style.display = 'flex';
 })
