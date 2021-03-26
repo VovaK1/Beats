5e58f11c6b1e34fc98a2bea121c8d83adfb3db34
Vova Kozak <volodymyr.kozak111@gmail.com>
2021-03-26 08:12:53 +0200

    accordion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//hamburger
+
 const fullMenu = document.querySelector(".nav--full");
 const hamburger = document.querySelector(".hamburger");
 
@@ -13,3 +15,38 @@ hamburger.addEventListener('click', e => {
   hamburger.classList.add('hamburger__active');
   }
 })
+
+//accordion
+
+const openItem = item => {
+  const container = item.closest('.team__member');
+  const contentBlock = container.find('.team__member-content');
+  const textBlock = contentBlock.find('.team__member-content-block');
+  const reqHeight = textBlock.height();
+
+
+  container.addClass('active');
+  contentBlock.height(reqHeight);
+};
+
+const closeEveryItem = container => {
+  const items = container.find('.team__member-content');
+  const itemsContainer = container.find('.team__member');
+
+  itemsContainer.removeClass('active');
+  items.height(0);
+};
+
+$('.team__member-name').on('click', e => {
+  const $this = $(e.currentTarget);
+  const container = $this.closest('.team__list');
+  const itemContainer = $this.closest('.team__member');
+
+  if (itemContainer.hasClass('active')) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItem($this);
+  }
+})
+
